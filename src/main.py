c1a7cec4ed0c0142d9995471e950b9aaeabbb3bf
azka1415 <rafifzaydan4@gmail.com>
2023-02-07 11:06:31 +0700

    :sparkles: added middleware

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 from src.routes import task
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(task.router)
 
